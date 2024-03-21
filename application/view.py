a9b3052fa9052de85ad381611dae7de253932108
Mark Luna (PH 74677) <allenluna3799@gmail.com>
2024-03-22 07:28:39 +0800

    Auto download cv

--- application/view.py
+++ application/view.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from .controller import SeleniumDriver
+
+
 view = Blueprint("view", __name__)
 
 
@@ -11,18 +13,33 @@ def home():
 
 @view.route("/filter-data", methods=["GET", "POST"])
 def filter_data():
-    
+    # Access data from local storage sent from client-side
+    local_data = request.json.get("localData", None)
     if request.method == "POST":
-        filter = request.json["filter"]
-        if filter != "":
-            # get generated account
-            sig_in = request.json["sign_in"]
-            password = request.json["password"]
-            
-            open_linkedin = SeleniumDriver()
-            open_linkedin.login(sig_in, password)
-            print("works")
-        else:
-            return {"message": "You need to search"}
-    
-    return {"message": "Filtered"}
+        try:
+            # Get the JSON data from the request body
+            request_data = request.get_json()
+            filter = request_data.get("filter", "")
+            if filter != "":
+                # get generated account
+                sig_in = request_data["sign_in"]
+                password = request_data["password"]
+                
+                open_linkedin = SeleniumDriver()
+                try:
+                    open_linkedin.login(sig_in, password)
+                    parsed_data = open_linkedin.filter_employee()
+                    # Store the parsed data in the local storage
+                    
+                    return jsonify({"message": "Resumes Downloaded", "data": parsed_data})
+                    
+                except TimeoutError:
+                    print("Error network")
+
+            else:
+                return {"message": "You need to search"}
+        except Exception as e:
+            print(f"Error: {e}")
+
+    return {"message": local_data}
+
